7b606dba78db957041d5ac3bc9fe31bc0b8d3534
SuperDumbTM <eltonlochunkit@gmail.com>
2024-01-05 21:01:15 +0800

    Add comment

--- app/config/flask_config.py
+++ app/config/flask_config.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
 
+# Directories
+
 CONFIG_DIR = Path(__file__).parent.parent.joinpath("config", "data")
 """Path to the directory for configuration files"""
 
 CACHE_DIR = Path(__file__).parent.parent.joinpath("caches")
 """Path to the directory for cache files"""
+
+# Paths
 
 ENV_FILE_PATH = Path(__file__).parent.parent.parent.joinpath(".env")
 """Path to `.env` file"""
