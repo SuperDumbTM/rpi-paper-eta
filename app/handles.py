13f0ac91d58202a5157880b519c87f90877bbe46
SuperDumbTM <eltonlochunkit@gmail.com>
2024-01-08 22:20:39 +0800

    Error handling

--- app/handles.py
+++ app/handles.py
@@ -1,7 +1,16 @@
+import logging
+import traceback
+
 import marshmallow
-from flask import Blueprint, jsonify
+from flask import Blueprint, current_app, jsonify, render_template, request
+from werkzeug.exceptions import HTTPException
+from flask_babel import lazy_gettext
 
 bp = Blueprint('error_handlers', __name__)
+
+
+def _need_json_response():
+    return request.accept_mimetypes.accept_json and '/api/' in request.path
 
 
 @bp.app_errorhandler(marshmallow.ValidationError)
@@ -15,3 +24,42 @@ def handle_validation_error(err: marshmallow.ValidationError):
             'errors_at': location
         }
     }), 400
+
+
+@bp.app_errorhandler(404)
+def error_handler_404(e: HTTPException):
+    if _need_json_response():
+        return jsonify({
+            'success': False,
+            'message': 'The requested URL was not found on the server.',
+            'data': None
+        }), 404
+    return render_template('error.jinja', error=e, code=404, msg=e.description)
+
+
+@bp.app_errorhandler(HTTPException)
+def error_handler_all(e: HTTPException):
+    if _need_json_response():
+        return jsonify({
+            'success': False,
+            'message': e.description,
+            'data': None
+        }), e.code
+    return render_template('error.jinja', error=e, code=e.code, msg=e.description)
+
+
+@bp.app_errorhandler(Exception)
+def error_handler_all(e: Exception):
+    logging.exception(str(e))
+    if _need_json_response():
+        return jsonify({
+            'success': False,
+            'message': 'Unexpected internal server error.',
+            'data': None if not current_app.debug else {
+                'type': e.__class__.__name__,
+                'error_message': str(e),
+                'traces': traceback.format_exception(e)[1:-1]
+            }
+        }), 500
+    return render_template(
+        'error.jinja', error=e, code=500, msg=lazy_gettext('unexpected_error_occured'))
