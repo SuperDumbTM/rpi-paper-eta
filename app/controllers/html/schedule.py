c1fb6305faf1621b1ec52be5ffd1130423b655a0
SuperDumbTM <eltonlochunkit@gmail.com>
2024-01-08 23:18:28 +0800

    Schedule import

--- app/controllers/html/schedule.py
+++ app/controllers/html/schedule.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, redirect, render_template, url_for
-from app import config, forms
+import json
+import logging
 
+from flask import (Blueprint, Response, flash, redirect, render_template,
+                   request, url_for)
+from flask_babel import lazy_gettext
+
+from app import config, enums, forms
+from app.config import site_data
 from app.modules import image as eimage
-
 
 bp = Blueprint('schedule',
                __name__,
@@ -56,3 +61,39 @@ def edit(id: str):
                            layouts=eimage.eta_image.EtaImageGeneratorFactory.get_generator(
                                conf.epd_brand, conf.epd_model).layouts()
                            )
+
+
+@bp.route('/export')
+def export():
+    scheduler = config.site_data.RefreshSchedule()
+    return Response(
+        json.dumps(
+            tuple(map(lambda s: s.model_dump(
+                exclude=['id', 'enabled']), scheduler.get_all())),
+            indent=4),
+        mimetype='application/json',
+        headers={'Content-disposition': 'attachment; filename=schedule.json'})
+
+
+@bp.route('/import', methods=['POST'])
+def import_():
+    try:
+        file = request.files['schedule']
+
+        scheduler = site_data.RefreshSchedule()
+        for schedule in json.load(file.stream):
+            try:
+                scheduler.create(schedule['schedule'],
+                                 schedule['eta_type'],
+                                 schedule['layout'],
+                                 schedule['is_partial'],
+                                 False)
+                # TODO: validation
+            except KeyError:
+                logging.exception(
+                    'Encountering missing field(s) during refresh schedule import.')
+                pass
+    except (UnicodeDecodeError, json.decoder.JSONDecodeError) as e:
+        print(e)
+        flash(lazy_gettext('import_failed'), enums.FlashCategory.error)
+    return redirect(url_for('schedule.schedules'))
