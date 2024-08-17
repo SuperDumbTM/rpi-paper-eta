58c29f5f42be8364e832c9ef1a87004ad513873c
SuperDumbTM <eltonlochunkit@gmail.com>
2024-08-17 23:53:08 +0800

    Use gthread w/w 4 threads

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,7 @@
 wsgi_app = "paper_eta.main:create_app()"
 proc_name = "rpi-paper-eta"
 bind = "0.0.0.0:8192"
-workers = 2
-worker_class = "sync"
+threads = 4
+worker_class = "gthread"
 timeout = 90
 accesslog = "-"
-preload_app = True
